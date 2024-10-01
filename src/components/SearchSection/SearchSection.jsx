ce36dc529a0085e58aee58bf6b9e8aa2acb54f0c
Anton Morozov <symfosium@gmail.com>
2024-10-01 11:20:15 +0300

    removed searchSection component from forms

--- src/components/SearchSection/SearchSection.jsx
+++ src/components/SearchSection/SearchSection.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import './SearchSection.css'
 
-function SearchSection({ searchQuery, onSearchChange, onClearSearch }) {
-
+function SearchSection({
+  searchQuery,
+  onSearchChange,
+  onClearSearch,
+  hideInput,
+}) {
   return (
     <div className="search-section">
       <div className="input-container">
-        <input
-          type="text"
-          placeholder="Search by name"
-          className="search-input"
-          value={searchQuery}
-          onChange={onSearchChange}
-        />
-        {searchQuery && (
-          <button className="clear-button" onClick={onClearSearch}>
-            Clear
-          </button>
+          {!hideInput && (
+          <>
+            <input
+              type="text"
+              placeholder="Search by name"
+              className="search-input"
+              value={searchQuery}
+              onChange={onSearchChange}
+            />
+            {searchQuery && (
+              <button className="clear-button" onClick={onClearSearch}>
+                Clear
+              </button>
+            )}
+          </>
         )}
       </div>
     </div>
